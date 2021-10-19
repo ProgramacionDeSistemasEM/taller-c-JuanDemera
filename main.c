e016644b8212d73139d5b512077b8fbfc9c52f44
Juan Demera <jpdemera@espol.edu.ec>
2021-10-19 15:36:20 -0500

    fin

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#hola
+//#hola
 int main(int argc, char **argv){
 	
 	int elementos = 0;
@@ -9,6 +9,9 @@ int main(int argc, char **argv){
 	
 	
 	//Aqui manejamos las opciones.
+////	printf("ingrese su peso");
+//	scanf("%.2f",&elemento)
+	//	
 	//El argumento -p tiene un argumento, que es 
 	//el numero de elementos a pedir (por eso el :)
 	while ((c = getopt (argc, argv, "p:")) != -1){
@@ -26,10 +29,20 @@ int main(int argc, char **argv){
 	//Puede declarar más variables si lo necesita.
 	float sum = 0.0f;
 	float max_imc = 0.0f;
-	
-	
-	
-	
+	float variable=0.0f;
+	float variableH=0.0f;
+	for(int i; i<10;i++);{
+		printf("ingrese peso:");
+		scanf("%.2f",&variable);
+		
+		printf("ingrese Altura:");
+		scanf("%.2f",&variableH);
+		if(max_imc<(variable/(variableH*variableH)){
+				max_imc=(variable/(variableH*variableH));
+				}
+				
+		sum= max_imc+(variable/(variableH*variableH));
+		}
 	//No modifique estas lineas
 	//Guarde los resultados en las 
 	//variables sum y max_imc
